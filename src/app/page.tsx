e779333deb0061f70564e68399bd2bcb7a3416e6
Briaharrold <94560548+Briaharrold@users.noreply.github.com>
2024-05-07 00:03:12 -0700

    idk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ import { Navbar } from "flowbite-react";
 import logo from "../../public/assets/New Project.png";
 import name from "../../public/assets/Bria.png";
 import tsIcon from "../../public/assets/ts.webp";
-import weatherApp from "../../public/assets/WetherApp.png"
+import weatherApp from "../../public/assets/WetherApp.png";
 import jsIcon from "../../public/assets/js.webp";
-import pokeDex from "../../public/assets/Image 2-5-24 at 8.47 AM.jpg"
+import pokeDex from "../../public/assets/Image 2-5-24 at 8.47 AM.jpg";
 import htmlIcon from "../../public/assets/html-1 copy 2.png";
 import matIcon from "../../public/assets/material.webp";
 import bootIcon from "../../public/assets/bs.webp";
@@ -47,13 +47,13 @@ export default function Home() {
             <Navbar.Link
               as={Link}
               className="text-black text-xl hover:!text-purple-400  hover:underline decoration-wavy underline-offset-8"
-              href="#"
+              href="#myprodjects"
             >
               Projects
             </Navbar.Link>
             <Navbar.Link
               className="text-black text-xl mr-0 lg:mr-20 hover:!text-purple-400  hover:underline decoration-wavy underline-offset-8"
-              href="#"
+              href="#letsConnect"
             >
               Let's Connect
             </Navbar.Link>
@@ -69,47 +69,49 @@ export default function Home() {
                 Junior Web Developer
               </p>
               <p className="text-black text-xl jFont  ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id esse
-                corporis mollitia, similique consectetur itaque asperiores
-                consequatur laudantium nihil, exercitationem commodi ullam
-                tempore reprehenderit nam, illum rem ex saepe harum.
+                I am currently pursuing my passion for software engineering at
+                CodeStack Academy, where I have been immersed in a comprehensive
+                curriculum since September. My journey in this field reflects my
+                dedication to mastering the skills necessary to thrive in the
+                dynamic world of technology. I am excited to apply my knowledge
+                and creativity to develop innovative solutions that make a
+                positive impact.
               </p>
               <div className="bg-red-300 px-2 py-2 mt-4 mb-4 lg:mt-2 lg:mb-0 rounded-lg">
-              
-              <h1 className="text-center text-4xl pt-2 pb-2 underline  ">
-                {" "}
-                My Skill Sets
-              </h1>
-              <div className="flex flex-wrap  flex-col-3 justify-evenly  order-last   rounded-xl px-0 py-0 lg:px-10 lg:py-10">
-                <div>
-                  <h1 className="text-center text-2xl">Frameworks:</h1>
-                  <div className=" flex flex-row flex-wrap gap-3 pr-2   pb-5 lg:pb-0 lg:pt-2 pt-5 ">
-                    <img src={bootIcon.src} className="h-8"></img>
+                <h1 className="text-center text-4xl pt-2 pb-2 underline  ">
+                  {" "}
+                  My Skill Sets
+                </h1>
+                <div className="flex flex-wrap  flex-col-3 justify-evenly  order-last   rounded-xl px-0 py-0 lg:px-10 lg:py-10">
+                  <div>
+                    <h1 className="text-center text-2xl">Frameworks:</h1>
+                    <div className=" flex flex-row flex-wrap gap-3 pr-2   pb-5 lg:pb-0 lg:pt-2 pt-5 ">
+                      <img src={bootIcon.src} className="h-8"></img>
 
-                    <img src={tailIcon.src} className="h-8 hover:"></img>
+                      <img src={tailIcon.src} className="h-8 hover:"></img>
+                    </div>
                   </div>
-                </div>
-                <div>
-                  <h1 className="text-center text-2xl ">Libraries:</h1>
-                  <div className=" flex flex-row flex-wrap gap-3 pr-2 pb-5 lg:pb-0 lg:pt-2 pt-5">
-                    <img src={reactIcon.src} className="h-8"></img>
+                  <div>
+                    <h1 className="text-center text-2xl ">Libraries:</h1>
+                    <div className=" flex flex-row flex-wrap gap-3 pr-2 pb-5 lg:pb-0 lg:pt-2 pt-5">
+                      <img src={reactIcon.src} className="h-8"></img>
 
-                    <img src={flowbiteIcon.src} className="h-8"></img>
+                      <img src={flowbiteIcon.src} className="h-8"></img>
 
-                    <img src={matIcon.src} className="h-8"></img>
+                      <img src={matIcon.src} className="h-8"></img>
+                    </div>
                   </div>
-                </div>
-                <div>
-                  <h1 className="text-center text-2xl ">Languages:</h1>
-                  <div className=" flex flex-row flex-wrap gap-3 pr-2  pb-5 lg:pb-0 lg:pt-2 pt-5">
-                    <img src={jsIcon.src} className="h-8"></img>
+                  <div>
+                    <h1 className="text-center text-2xl ">Languages:</h1>
+                    <div className=" flex flex-row flex-wrap gap-3 pr-2  pb-5 lg:pb-0 lg:pt-2 pt-5">
+                      <img src={jsIcon.src} className="h-8"></img>
 
-                    <img src={tsIcon.src} className="h-8"></img>
+                      <img src={tsIcon.src} className="h-8"></img>
 
-                    <img src={CIcon.src} className="h-8"></img>
-                    <img src={htmlIcon.src} className="h-8"></img>
+                      <img src={CIcon.src} className="h-8"></img>
+                      <img src={htmlIcon.src} className="h-8"></img>
+                    </div>
                   </div>
-                </div>
                 </div>
               </div>
             </div>
@@ -122,9 +124,14 @@ export default function Home() {
           </div>
         </div>
         <div className="">
-          <h1 className="text-center pb-5 text-5xl text underline">Projects</h1>
-          <div className="flex flex-wrap justify-evenly">
-            <div>
+          <h1
+            className="text-center pb-5 text-5xl text underline"
+            id="myprodjects"
+          >
+            Projects
+          </h1>
+          <div className="flex flex-wrap justify-evenly pb-5">
+            <div className="">
               <Card
                 className="max-w-sm px-5 py-5 mt-5 mb-5"
                 renderImage={() => (
@@ -137,12 +144,31 @@ export default function Home() {
                   />
                 )}
               >
-                <h5 className="text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white hover:!text-purple-400  hover:underline  underline-offset-8">
-                 PokeDex
-                </h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400">
-                
-                </p>
+                <a href="pokemonremake2.vercel.app"  target="_blank" rel="noopener noreferrer"  className="text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white hover:!text-purple-400 cursor-pointer  hover:underline  underline-offset-8">
+                  PokeDex
+                </a>
+                <p className="font-normal text-gray-700 dark:text-gray-400"></p>
+              </Card>
+            </div>
+            <div>
+              <Card
+                className="max-w-sm px-5 py-5 mt-5 mb-5"
+                renderImage={() => (
+                  <Image
+                    width={500}
+                    height={500}
+                    className="rounded-xl"
+                    src={weatherApp.src}
+                    alt="image 1"
+                  
+                    
+                  />
+                )}
+              >
+                <a  href="weatherapp-eta-tan.vercel.app"  target="_blank" rel="noopener noreferrer" className="text-2xl text-center font-bold tracking-tight cursor-pointer text-gray-900 dark:text-white hover:!text-purple-400  hover:underline  underline-offset-8">
+                  Weather Application
+                </a>
+                <p className="font-normal text-gray-700 dark:text-gray-400"></p>
               </Card>
             </div>
             <div>
@@ -158,40 +184,31 @@ export default function Home() {
                   />
                 )}
               >
-                <h5 className="text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white hover:!text-purple-400  hover:underline  underline-offset-8">
-                 Weather Application
-                </h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400">
-                
-                </p>
-              </Card>
-            </div>
-            <div>
-            <Card
-                className="max-w-sm px-5 py-5 mt-5 mb-5"
-                renderImage={() => (
-                  <Image
-                    width={500}
-                    height={500}
-                    className="rounded-xl"
-                    src={weatherApp.src}
-                    alt="image 1"
-                  />
-                )}
-              >
-                <h5 className="text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white hover:!text-purple-400  hover:underline  underline-offset-8">
-                 Weather Application
-                </h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400">
-                
-                </p>
+                <a className="text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white cursor-pointer hover:!text-purple-400  hover:underline  underline-offset-8">
+                  Weather Application
+                </a>
+                <p className="font-normal text-gray-700 dark:text-gray-400"></p>
               </Card>
             </div>
           </div>
         </div>
       </div>
-      <div className="bg-red-300">
-<h1></h1>
+      <div className="bg-red-300 px-10 py-10 jFont">
+        <div className="flex flex-col-2 justify-between">
+          <div>
+            <p id="letsConnect" className=" text-4xl font-bold underline">
+              Let's Connect
+            </p>
+            <p>Bria Harrold</p>
+            <p>209-292-1954</p>
+            <h1>Briaharrold@gmail.com</h1>
+          </div>
+          <div>
+            <button> Github</button>
+            <button> Github</button>
+            <button> Github</button>
+          </div>
+        </div>
       </div>
     </>
   );
