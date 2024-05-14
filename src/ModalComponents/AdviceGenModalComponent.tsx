f3cb8898cc61d9472b6553d6d5ec3701018d9cd3
Briaharrold <94560548+Briaharrold@users.noreply.github.com>
2024-05-13 17:42:43 -0700

    File Structure

---
+++ src/ModalComponents/AdviceGenModalComponent.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const AdviceGenModalComponent = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AdviceGenModalComponent
---
+++ src/ModalComponents/PokemonModalComoponent.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const PokemonModalComoponent = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PokemonModalComoponent
